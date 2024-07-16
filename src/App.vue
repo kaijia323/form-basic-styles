5c70aa5ae7e8eaeee6585f195625d834db6486c6
kaijia <1759555202@qq.com>
2024-07-16 10:05:56 +0800

    按钮部分样式

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from "vue";
+const buttonAttrs = ref({
+  width: "fit-content",
+  height: "fit-content",
+  showBorder: true,
+  color: "",
+  backgroundColor: "",
+});
+
+const turnPx = (num?: string | number) => {
+  if (isNaN(Number(num))) {
+    return num;
+  }
+  return num + "px";
+};
+
+const border = computed(() => {
+  if (buttonAttrs.value.showBorder) {
+    return "";
+  }
+  return "none";
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="form-basic-styles">
+    <button
+      :style="{
+        ...buttonAttrs,
+        width: turnPx(buttonAttrs.width),
+        height: turnPx(buttonAttrs.height),
+        border,
+      }"
+    >
+      按钮样式
+    </button>
+    <div class="layout-right">
+      <el-form :model="buttonAttrs">
+        <el-form-item label="宽度" prop="width">
+          <el-input-number v-model="buttonAttrs.width"></el-input-number>
+        </el-form-item>
+        <el-form-item label="高度" prop="height">
+          <el-input-number v-model="buttonAttrs.height"></el-input-number>
+        </el-form-item>
+        <el-form-item label="是否显示边框" prop="border">
+          <el-checkbox v-model="buttonAttrs.showBorder"></el-checkbox>
+        </el-form-item>
+        <el-form-item label="文字颜色" prop="color">
+          <el-color-picker v-model="buttonAttrs.color" />
+        </el-form-item>
+        <el-form-item label="背景颜色" prop="backgroundColor">
+          <el-color-picker v-model="buttonAttrs.backgroundColor" />
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style lang="scss" scoped>
+.form-basic-styles {
+  display: flex;
+  .layout-right {
+    margin-left: auto;
+  }
 }
 </style>
