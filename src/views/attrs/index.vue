d1480a7096d21c7292189d2baa603ca5d4289847
kaijia <1759555202@qq.com>
2024-07-17 11:40:30 +0800

    目录结构调整

--- src/views/attrs/index.vue
+++ src/views/attrs/index.vue
@@ -1,37 +1,33 @@
 <template>
   <div class="attrs-wrapper">
-    <aside>
-      <div class="aside-container">
-        <span>按钮</span>
-      </div>
-    </aside>
-    <main>
-      <ButtonCom />
-    </main>
+    <el-aside class="cz-aside">
+      <el-menu default-active="2" class="el-menu-vertical-demo">
+        <el-scrollbar>
+          <el-menu-item v-for="index in 30" :key="index">
+            <span>按钮 Button {{ index }}</span>
+          </el-menu-item>
+        </el-scrollbar>
+      </el-menu>
+    </el-aside>
+    <el-main>
+      <ButtonCom :styles="styles" />
+    </el-main>
+    <el-aside class="config-aside">
+      <el-scrollbar>
+        <CzForm v-model="attrs"></CzForm>
+      </el-scrollbar>
+    </el-aside>
   </div>
 </template>
 
 <script lang="ts" setup>
 import ButtonCom from "./button/index.vue";
+import CzForm from "@/components/czForm.vue";
+import { useButtonAttrs } from "./button/useButtonAttrs";
+
+const { attrs, styles } = useButtonAttrs();
 </script>
 
 <style lang="scss" scoped>
-.attrs-wrapper {
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  gap: 24px;
-
-  aside {
-    height: 100%;
-    width: 200px;
-    background: #c6dfc8;
-  }
-
-  main {
-    display: flex;
-    flex: 1;
-  }
-}
+@import "./index.scss";
 </style>
