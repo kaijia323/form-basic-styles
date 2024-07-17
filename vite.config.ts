68818e670f4a3e89f1b71d5ec9c3b973b22f284c
kaijia <1759555202@qq.com>
2024-07-17 10:03:29 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { resolve } from "node:path";
+
+const __dirname = resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +21,10 @@ export default defineConfig({
   ],
   server: {
     port: 5180,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
   },
 });
